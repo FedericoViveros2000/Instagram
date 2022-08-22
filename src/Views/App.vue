882fc057a7b940fa0355fcb6200c180ef8d71454
Jose Federico Viveros Marin <josevivers2000@gmail.com>
2022-08-22 16:44:14 -0400

    Agregado Beta del Splash screen de carga

--- src/Views/App.vue
+++ src/Views/App.vue
@@ -1,20 +1,37 @@
 <template>
-    <search-bar-component></search-bar-component>  
-    <router-view v-slot="{ Component }">
-      <Transition name="scale-slide">
-        <component :is="Component" />
-      </Transition>
-    </router-view>
-    <nav-bar-component></nav-bar-component>
+  <Transition
+    enter-from-class="opacity-0"
+    enter-active-class="transition-opacity duration-500 linear"
+    enter-to-class="opacity-1"
+    leave-from-class="opacity-1"
+    leave-active-class="transition-opacity duration-500 linear"
+    leave-to-class="opacity-0"
+  >
+    <splash-screen v-if="show"></splash-screen>
+    <div class="w-full" v-else>
+      <search-bar-component></search-bar-component>  
+      <router-view v-slot="{ Component }">
+        <Transition name="scale-slide">
+          <component :is="Component" />
+        </Transition>
+      </router-view>
+      <nav-bar-component></nav-bar-component>
+    </div>
+  </Transition>
+    <!-- <splash-screen></splash-screen> -->
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import navBarComponent from "../components/NavBarComponent.vue";
 import splashScreen from "../components/skeletons/SplashScreen.vue"
 import searchBarComponent from "../components/SearchBarComponent.vue";
+let show = ref(true);
 onMounted(() => {
   localStorage.mode === 'true' ? document.documentElement.classList.add("dark") : document.documentElement.classList.remove("dark");
+  setTimeout(()=> {
+    show.value = false;
+  }, 3000)
 })
 </script>
 
--- src/components/skeletons/SplashScreen.vue
+++ src/components/skeletons/SplashScreen.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="w-full h-screen bg-white flex flex-col justify-center items-center">
-    <figure class="overflow-hidden w-20">
-        <img src="../assets/instagram.png" class="w-full object-cover" alt="Logo de carga">
+  <div class="w-full h-screen bg-white dark:bg-black">
+    <figure class="w-full h-1/2 flex flex-col justify-end items-center">
+        <img src="../assets/instagram_screen.png" class="w-16 object-cover" alt="Logo de carga">
     </figure>
+    <div class="w-full h-1/2 flex flex-col justify-end items-center pb-10">
+      <p class="text-center text-black dark:text-white text-xl">Made in JosFe</p>
+    </div>
   </div>
 </template>
