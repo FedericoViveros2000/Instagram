6e001f9ae1a376529db3c59d630b8d17d08d2fb0
git stash <josevivers2000@gmail.com>
2022-11-07 15:18:53 -0300

    index on (no branch): 9870f0c Seccion de perfil modificada

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,4 +1,5 @@
 <template>
+<div class="w-full">
   <div class="w-full py-3 text-2xl bg-white dark:bg-black fixed z-30 bottom-0 right-0 flex justify-between items-center px-5">
     <router-link to="/Instagram/">
       <figure>
@@ -18,4 +19,31 @@
       <img src="./assets/user.png" class="w-8 h-8 object-cover outline-0 ring-0 rounded-full" alt="Perfil">
     </router-link> 
   </div>
+  <div class="w-full bg-slate-800 rounded-lg py-5 px-5 fixed bottom-0 z-50" v-if="showInstall">
+    <p class="text-center">¿Desea instalar la aplicación?</p>
+    <p class="w-full bg-skyblue text-white text-center rounded-md py-1.5 my-5" @click="installPWA">Instalar</p>
+    <p class="w-full text-white text-center rounded-md py-1">Cancelar</p>
+  </div>
+</div>
 </template>
+
+<script setup>
+  import {onMounted, ref} from "vue";
+  let showInstall = ref(false);
+  let userInstall = ref("");
+  const installPWA = async () => {
+    userInstall.prompt();
+    let accept = await userInstall.value.userChoice;
+    if (accept.outcome === 'accepted') {
+      
+    }
+  }
+  onMounted(() => {
+    window.addEventListener('beforeinstallprompt', e => {
+      e.preventDefault();
+      userInstall.value = e;
+      showInstall.value = true;
+      console.log(showInstall.value);
+    })
+  })
+</script>
